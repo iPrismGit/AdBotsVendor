ffaa7493d523c2d7375c4b0f0935e89308ae592a
Omprakash <omprakash.mopuri@improve10x.com>
2026-03-24 16:04:45 +0530

    Integrated the Montserrat font family and updated the app's typography system.
    - Added Montserrat font assets for various weights (Thin, Light, Regular, Medium, SemiBold, Bold, ExtraBold, Black).
    - Defined `MontserratFamily` in `Type.kt` to map font weights to the new assets.
    - Fully overhauled the `Typography` configuration to use Montserrat across all Material3 text styles, including display, headline, title, body, and label categories.
    - Customized specific font sizes, weights, and letter spacing for enhanced UI consistency.

--- app/src/main/java/com/iprism/adbotsvendor/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/iprism/adbotsvendor/presentation/ui/theme/Type.kt
@@ -2,33 +2,116 @@ package com.iprism.adbotsvendor.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.iprism.adbotsvendor.R
+
+
+val MontserratFamily = FontFamily(
+    Font(R.font.montserrat_regular, FontWeight.Normal),
+    Font(R.font.montserrat_bold, FontWeight.Bold),
+    Font(R.font.montserrat_light, FontWeight.Light),
+    Font(R.font.montserrat_thin, FontWeight.Thin),
+    Font(R.font.montserrat_semibold, FontWeight.SemiBold),
+    Font(R.font.montserrat_black, FontWeight.Black),
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+    Font(R.font.montserrat_extrabold, FontWeight.ExtraBold),
+    Font(R.font.montserrat_extrabold, FontWeight.ExtraLight),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+
+    // For big Texts
+
+    displayLarge = TextStyle(
+        fontFamily = MontserratFamily,
+        fontSize = 96.sp,
+        fontWeight = FontWeight.Light,
+        lineHeight = 117.sp,
+        letterSpacing = (-1.5).sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = MontserratFamily,
+        fontSize = 60.sp,
+        fontWeight = FontWeight.Light,
+    ),
+    displaySmall = TextStyle(
+        fontFamily = MontserratFamily,
+        fontSize = 48.sp,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
+
+    // For Headlines
+
+    headlineLarge =  TextStyle(
+        fontFamily = MontserratFamily,
+        fontSize = 30.sp,
+        fontWeight = FontWeight.SemiBold,
+    ),
+
+    headlineMedium = TextStyle(
+        fontFamily = MontserratFamily,
+        fontSize = 30.sp,
+        fontWeight = FontWeight.SemiBold,
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = MontserratFamily,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.SemiBold,
+    ),
+
+    // For Text Titles
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = MontserratFamily,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.SemiBold,
+    ),
+    titleMedium = TextStyle(
+        fontFamily = MontserratFamily,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Bold,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = MontserratFamily,
+        fontSize = 14.sp,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    ),
+
+    // For TextFields
+
+    bodyLarge = TextStyle(
+        fontFamily = MontserratFamily,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Medium,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = MontserratFamily,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Normal,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = MontserratFamily,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Normal
+    ),
+
+    // For Buttons
+    labelMedium = TextStyle(
+        fontFamily = MontserratFamily,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Bold,
+    ),
+    labelLarge = TextStyle(
+        fontFamily = MontserratFamily,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Bold,
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = MontserratFamily,
+        fontSize = 12.sp,
+        fontWeight = FontWeight.SemiBold
     )
-    */
 )
