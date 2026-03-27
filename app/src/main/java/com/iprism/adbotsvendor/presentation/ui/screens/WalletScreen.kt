55ec84ced80c47de10342d20cc2ea7b8637dd7fe
Omprakash <omprakash.mopuri@improve10x.com>
2026-03-27 10:09:02 +0530

    Implemented the Notifications screen and integrated it into the app navigation.
    - Created `NotificationsScreen.kt` with a `LazyColumn` to display a list of notifications using dummy data.
    - Added `NotificationItem` and `NotificationData` to handle the display and structure of individual notifications.
    - Integrated the `Notifications` route into `AppNavHost` and `Screen.kt`.
    - Updated `HomeScreen` to navigate to the notifications screen when the notification icon is clicked and wrapped header icons in `IconButton` for better accessibility.
    - Added `LightGrey2` to the theme colors.

--- app/src/main/java/com/iprism/adbotsvendor/presentation/ui/screens/WalletScreen.kt
+++ app/src/main/java/com/iprism/adbotsvendor/presentation/ui/screens/WalletScreen.kt
@@ -6,16 +6,12 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.LightGray
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
